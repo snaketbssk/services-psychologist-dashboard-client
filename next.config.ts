0bd522d3d193a3b992a6e8085bdaf29dcb0cf0e2
Vladimir Tereshin <snaketbssk@gmail.com>
2026-03-29 09:09:40 +0400

    commit

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  //basePath: "/dashboard",
+  basePath: "/dashboard",
   // Add image remote patterns here when needed:
   // images: {
   //   remotePatterns: [{ protocol: "https", hostname: "example.com" }],
